7d33751434bbf745ed454786e1f2792a7c242e67
albseb511 <albertsebastian511@gmail.com>
2020-07-02 02:00:24 +0530

    Update: readme

--- src/Components/TaskApp.js
+++ src/Components/TaskApp.js
@@ -2,18 +2,54 @@ import React, {Component} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import TaskForm from './TaskForm'
 import TaskList from './TaskList'
-
+import {addTask, loadData} from "../utils/axios"
 
 export default class TodoApp extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      todos: []
+      newTaskName: "",
+      todos: [],
+      error: false
     }
   }
 
+  onInputChange = (e) => {
+    this.setState({
+      newTaskName: e.target.value
+    })
+  }
 
+  componentDidMount(){
+    loadData()
+    .then(({data})=> this.setState({
+      todos: data
+    }) )
+    .catch( err=> this.setState({
+      error: true
+    }) )
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    let payload = {
+      title: this.state.newTaskName
+    }
+    addTask(payload)
+    .then( ({data}) => {
+      this.setState({
+        todos: [ ...this.state.todos, data ]
+      })
+    })
+    .catch( err => {
+      this.setState({
+        error: true
+      })
+      console.log('error')
+    } )
+    
+  }
 
   render () {
     return (
@@ -21,7 +57,11 @@ export default class TodoApp extends Component {
         <div className="container-fluid text-center">
           <header className="p-2">
             <h1>Tasks</h1>
-            <TaskForm />
+            { this.state.error && <h3 className="error text-danger"> Oops, something went wrong </h3> }
+            <TaskForm 
+              value={this.state.newTaskName}
+              onChange={this.onInputChange}
+              onSubmit={this.onSubmit}  />
           </header>
           <section className="mt-2">
             <TaskList todos={this.state.todos} />
--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const TaskForm = props =>
-  <form>
+  <form onSubmit={props.onSubmit}>
     <input
       type='text'
+      autoFocus
+      value={props.value}
+      onChange={props.onChange}
       className="p-2 rounded border task-input"
       placeholder="Add something?"/>
   </form>
